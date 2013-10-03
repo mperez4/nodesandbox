287afe761f3eaa0676d83a677607e315c99fea5f
miguel <mperez4@saic.edu>
2013-10-02 19:23:37 -0500

    e

--- gpio_test/colorFader.js
+++ gpio_test/colorFader.js
@@ -1,17 +1,40 @@
 var gpio = require("pi-gpio");
 var piblaster = require('pi-blaster.js');
 
-var cRed = getRandomInt(0, 255);
-var cGreen = getRandomInt(0, 255);
-var cBlue = getRandomInt(0, 255);
+var cRed = Math.random();
+var cGreen = Math.random();
+var cBlue = Math.random();
+var changePerLoop = 0.05;
+var threshold = 0.1;
 
-var tRed = getRandomInt(0, 255);
-var tGreen = getRandomInt(0, 255);
-var tBlue = getRandomInt(0, 255);
+var tRed = Math.random();
+var tGreen = Math.random();
+var tBlue = Math.random();
 
 var signRed;
 var signGreen;
 var signBlue;
+
+  if (cRed > tRed) {
+    signRed = -changePerLoop;
+  } 
+  else {
+    signRed = changePerLoop;
+  }
+
+  if (cGreen > tGreen) {
+    signGreen = -changePerLoop;
+  } 
+  else {
+    signGreen = changePerLoop;
+  }
+
+  if (cBlue > tBlue) {
+    signBlue = -changePerLoop;
+  } 
+  else {
+    signBlue = changePerLoop;
+  }
 
 var maxRed;
 var minRed;
@@ -19,6 +42,7 @@ var maxGreen;
 var minGreen;
 var maxBlue;
 var minBlue;
+var test = 0.9;
 
 setInterval(loop, 1);
 
@@ -26,56 +50,62 @@ function loop(){
     colorPick();
     
     // set R G B with current values
-    piblaster.setPwm(2, cRed);
-    piblaster.setPwm(3, cGreen);
-    piblaster.setPwm(4, cBlue);
-    console.log(cRed + "," + signRed);  
+    piblaster.setPwm(0, cRed);
+    piblaster.setPwm(2, cGreen);
+    piblaster.setPwm(3, cBlue);
+    console.log("The value of cRed is: " + cRed);
+    console.log("The value of cGreen is: " + cGreen);
+    console.log("The value of cBlue is: " + cBlue);
+    var difference = Math.abs(cRed - tRed);
+    console.log("The difference between cRed and tRed is " + difference);
+    
+    
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function mapRange(value, low1, high1, low2, high2){
+    return low2 + (high2 - low2) * ((value - low1) / (high1 - low1));
 }
 
 function colorPick(){
 
-    cRed = cRed + (1 * signRed);
-    cGreen = cGreen + (1 * signGreen);
-    cBlue = cBlue + (1 * signBlue);
+    cRed += (1 * signRed);  
+    cGreen += (1 * signGreen);
+    cBlue += (1 * signBlue);
   
     // RED
     // check to see if current = target
-    if (int(cRed) == int(tRed)) {
-        tRed = getRandomInt(0, 255);
+    if (cRed == tRed) {
+        tRed = Math.floor(Math.random());
     }
     // decide to fade up or down, depending where target is
     if (cRed < tRed) {
-        signRed = 1;
+        signRed = changePerLoop;
     } else {
-      signRed = -1;
+      signRed = -changePerLoop;
     }
   
     // GREEN
     // check to see if current = target
-    if (int(cGreen) == int(tGreen)) {
-        tGreen = getRandomInt(0, 255);
+    if (cGreen == tGreen) {
+        tGreen = Math.floor(Math.random());
     }
     // decide to fade up or down, depending where target is
     if (cGreen < tGreen) {
-      signGreen = 1;
+      signGreen = changePerLoop;
     } else {
-      signGreen = -1;
+      signGreen = -changePerLoop;
     }
   
     // BLUE
     // check to see if current = target
-    if (int(cBlue) == int(tBlue)) {
-        tBlue = getRandomInt(0, 255);
+    if (cBlue == tBlue) {
+        tBlue = Math.floor(Math.random());
     }
     // decide to fade up or down, depending where target is
     if (cBlue < tBlue) {
-      signBlue = 1;
+      signBlue = changePerLoop;
     } else {
-      signBlue = -1;
+      signBlue = -changePerLoop;
     }
 
 }
