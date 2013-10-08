40fd66c8a1d9024fcb526083a9bb09adcf26a7db
miguel <mperez4@saic.edu>
2013-10-07 19:23:26 -0500

    stable

--- twitter_test/tweet_blink.js
+++ twitter_test/tweet_blink.js
@@ -1,3 +1,5 @@
+/* run node tweet_blink.js --ledoff -q "yourQuery"*/
+
 var argv = require('optimist')
 		   .usage('Usage: $0 --pin [num] --search [string] --speed [milliseconds] --ledoff')
 		   .default({'pin': 12,
@@ -11,9 +13,10 @@ var argv = require('optimist')
 
 var led = argv.pin;
 var track = argv.search;
-var timeOn = argv.speed;
+var timeOn = argv.speed;	
 var isOn = false; //keeps track if led is on and doesn't allow new connection to be opened if it is
-
+var i = 0;
+var startValue = 0; //light is off when .app starts
 var gpio = require("pi-gpio");
 var util = require('util'),
     twitter = require('twitter');
@@ -23,33 +26,69 @@ var twit = new twitter({
     access_token_key: '1230084602-UtUB4QdlhNkv1aLqrjS3eYoZ96APon5IhjOqBFt',
     access_token_secret: 'wscl1nV8gFO4kMhtJy7DjhQKkpJHB1fW5Jzb4RXZq8'
 });
-
+//----------------------------------------------------------------------
 twit.stream('filter', { track: track }, function(stream) {
 	//event
     stream.on('data', function(data) {
-    	if(output(data) &&
-    	   !isOn &&
-    	   !argv.ledoff){
+    	if(output(data) && !isOn && !argv.ledoff){
 	    	writePin(led, true);
 	    	isOn = true;
 	    	setTimeout(function(){
 		    	writePin(led, false);
 		    	isOn = false;
+		    	console.log(i);
 		    }, timeOn);
 		}
+		//if led is off. debug here
+		//console.log(i);			
+		if(i >= 3){
+			console.log("visualizing...");//will print of more tweets are comming in ONLY. doesnt really serve a purpose though unless multiple tweets come in... like if we search for obama or something.
+		}
+		if (output(data) && i == 2){//'i' should be '1' but its good troubleshooting at 2
+			console.log("GOT A TWEET!!");
+			//pwm visualization goes here:
+			//////////////////////////////////////////
+			/*
+			startValue = 1;			
+			piblaster.setPwm(2, startValue);
+    		piblaster.setPwm(3, startValue);
+    		piblaster.setPwm(4, startValue);    		
+    		if(startValue == 1){
+    			startValue -= .1; //decreasing rate
+    			}
+			if(startValue <= .1)
+			{
+				startValue += .1;
+			}
+    		*/
+			//////////////////////////////////////////
+			traffic();//times out to reset tweet count.
+		}		
 	});
+
 });
 
+//----------------------------------------------------------------------
 function output(data){
+	//if we receive data...
 	if(typeof data !== 'undefined' &&
 	   typeof data.user !== 'undefined'){
-		printFragment("Created at", data.created_at);
-		printFragment("Created by", data.user.name);
-		printFragment("Tweet:", data.text);
-		console.log("");
+		// printFragment("Created at", data.created_at);
+		// printFragment("Created by", data.user.name);
+		// printFragment("Tweet:", data.text);
+		 // console.log("");		 			 
+		 i++;//keeps tab on how many tweets are comming in. 
 		return true;
-	}else return false; //error receiving tweet	
+	}else return false; //error receiving tweet		
+	
 }
+	function traffic(){
+		setTimeout(function(){
+		   console.log("VISUALIZED...continue..");
+		   i = 0;//resets i to 0 so we can revisualize an incoming tweet when it is equal to +2 (or 1 if we wanted to)
+		   startValue = 1;// turns light on
+			}, 2000);
+		}
 
 function printFragment(prefixMessage, value){
 	if(typeof value !== 'undefined') console.log(prefixMessage+" "+value);
